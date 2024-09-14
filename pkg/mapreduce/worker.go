1c0e607b5152d96ef96acfb33c1b58ceb88a3326
niziolekpaul <niziolek.paul@gmail.com>
2024-09-14 17:18:20 -0400

    Worker processMapTask implementation

--- pkg/mapreduce/worker.go
+++ pkg/mapreduce/worker.go
@@ -1,6 +1,7 @@
 package mapreduce
 
 import (
+	"encoding/json"
 	"fmt"
 	"hash/fnv"
 	"io"
@@ -34,7 +35,11 @@ func Worker(mapf func(string, string) []KeyValue,
 
 	for {
 		resp := &GetTaskResponse{}
-		call("Master.GetTask", &GetTaskRequest{}, resp)
+		success := call("Master.GetTask", &GetTaskRequest{}, resp)
+		if !success {
+			// Assume master has finished
+			os.Exit(0)
+		}
 		t := resp.Task
 
 		if t.TaskType == task.Map {
@@ -51,6 +56,8 @@ func Worker(mapf func(string, string) []KeyValue,
 
 func processMapTask(mapf func(string, string) []KeyValue, t *task.Task) {
 	filename := t.MapMetadata.InputFile
+	reduceWorkers := t.MapMetadata.ReduceWorkers
+
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatalf("cannot open %v", filename)
@@ -63,12 +70,48 @@ func processMapTask(mapf func(string, string) []KeyValue, t *task.Task) {
 
 	kva := mapf(filename, string(content))
 
-	// TODO: process KV pairs and emit to intermediate files
+	kvBuckets := createKVBuckets(kva, reduceWorkers)
+
+	intermediateFiles, _ := writeIntermediateFiles(t.ID, kvBuckets)
+
+	call("Master.FinishTask",
+		&FinishTaskRequest{
+			Task:       t,
+			FileOutput: intermediateFiles,
+		},
+		&FinishTaskResponse{},
+	)
+}
+
+func createKVBuckets(kva []KeyValue, reduceWorkers int) [][]*KeyValue {
+	buckets := make([][]*KeyValue, reduceWorkers)
+	for _, kv := range kva {
+		bucket := ihash(kv.Key) % reduceWorkers
+		buckets[bucket] = append(buckets[bucket], &kv)
+	}
+	return buckets
+}
+
+func writeIntermediateFiles(mapID int, buckets [][]*KeyValue) ([]string, error) {
+	createdFiles := make([]string, len(buckets))
+
+	for i, bucket := range buckets {
+		createdFiles[i] = fmt.Sprintf(intermediateFileFormat, mapID, i)
+		file, _ := os.Create(createdFiles[i])
+		enc := json.NewEncoder(file)
+		defer file.Close()
+
+		for _, kv := range bucket {
+			enc.Encode(kv)
+		}
+	}
+
+	return createdFiles, nil
 }
 
 func processReduceTask(reducef func(string, []string) string, t *task.Task) {
 	// TODO: IMPLEMENT
-
+	log.Fatalf("received reduce task, unimplemented...")
 }
 
 // send an RPC request to the master, wait for the response.
