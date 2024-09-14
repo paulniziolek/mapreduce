aedbdc6fca7e395f27cd75e2fd94ddb1f24bbccd
niziolekpaul <niziolek.paul@gmail.com>
2024-09-14 16:17:48 -0400

    Master GetTask logic- still needs improvements

--- pkg/mapreduce/master.go
+++ pkg/mapreduce/master.go
@@ -6,21 +6,81 @@ import (
 	"net/http"
 	"net/rpc"
 	"os"
+	"sync"
+	"time"
+
+	"github.com/paulniziolek/mapreduce/pkg/mapreduce/task"
 )
+
+const TaskTimeout = 10
 
 type Master struct {
 	// Your definitions here.
+	tasks []*task.Task
 
+	// asynchronously update tasks
+	tasklock sync.Mutex
+
+	// condition once all map tasks have been done
+	mapcond *sync.Cond
+
+	nReduce int
+
+	mapDone    bool
+	reduceDone bool
 }
 
-// Your code here -- RPC handlers for the worker to call.
+func (m *Master) GetTask(args *GetTaskRequest, reply *GetTaskResponse) error {
+	// look for map tasks
+	m.tasklock.Lock()
+	defer m.tasklock.Unlock()
 
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
-	return nil
+	for {
+		for _, t := range m.tasks {
+			if t.TaskType == task.Map && isProcessableTask(t) {
+				reply.Task = t
+				updateTask(t)
+				return nil
+			}
+		}
+
+		if m.mapDone {
+			for _, t := range m.tasks {
+				if t.TaskType == task.Reduce && isProcessableTask(t) {
+					reply.Task = t
+					updateTask(t)
+					return nil
+				}
+			}
+		}
+
+		if m.mapDone && m.reduceDone {
+			reply.Task = &task.Task{TaskType: task.Exit}
+			return nil
+		}
+
+		m.mapcond.Wait()
+	}
+}
+
+func isProcessableTask(t *task.Task) bool {
+	return t.Status == task.Idle || (t.Status == task.Processing && time.Now().Unix()-t.LastUpdated >= TaskTimeout)
+}
+
+func updateTask(t *task.Task) {
+	t.Status = task.Processing
+	t.LastUpdated = time.Now().Unix()
+}
+
+func (m *Master) FinishTask(args *FinishTaskRequest, reply *FinishTaskResponse) {
+	m.tasklock.Lock()
+	defer m.tasklock.Unlock()
+
+	args.Task.Status = task.Done
+	args.Task.LastUpdated = time.Now().Unix()
+
+	// TODO: IMPLEMENT
+
 }
 
 // start a thread that listens for RPCs from worker.go
@@ -40,20 +100,13 @@ func (m *Master) server() {
 // main/mrmaster.go calls Done() periodically to find out
 // if the entire job has finished.
 func (m *Master) Done() bool {
-	ret := false
-
-	// Your code here.
+	ret := m.mapDone && m.reduceDone
 
 	return ret
 }
 
-// create a Master.
-// main/mrmaster.go calls this function.
-// nReduce is the number of reduce tasks to use.
 func MakeMaster(files []string, nReduce int) *Master {
-	m := Master{}
-
-	// Your code here.
+	m := Master{nReduce: nReduce}
 
 	m.server()
 	return &m
